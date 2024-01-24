605793b0d93daf29c834b4a9307e220bd3fb8dbb
jeff1673 <157391412+jeff1673@users.noreply.github.com>
2024-01-24 16:05:42 +0000

    Did M1LAB

---
+++ M1T2_Fowe.cpp
@@ -0,0 +1,23 @@
+// CSC 134
+// M1T2
+// Jeff-Michael Fowe
+// 1/22/24
+
+#include <iostream>
+using namespace std;
+
+int main() {
+  cout << "Hello World!" << endl;
+  cout << "My name is Jeff-Michael Fowe. I am enrolled in the associate in science for engineering program. I enjoy playing soccer and video games." "\n";
+  cout << "I learn C++ programming" << endl;
+ 
+  // Ask a question
+  string name;
+  cout << "What's your name" << endl;
+  cin >> name;
+ 
+  cout << "Nice to meet you," << name << endl;
+  cout << endl << endl;
+  return 0;
+}
+
