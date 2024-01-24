6a5b58cac96dbd82b2b187bcc4a38e9e7427f329
jeff1673 <157391412+jeff1673@users.noreply.github.com>
2024-01-24 16:50:20 +0000

    HW complete

---
+++ M1HW1_Fowe.cpp
@@ -0,0 +1,22 @@
+/*
+CSC 134
+M1HW1
+Jeff-Michael Fowe
+02/24/24
+*/
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    string movie_name = "Spider-Man: Across the Spider-Verse";
+    int release_date = 2023;
+    double mon_gross = 6.9;
+    cout << "The movie " << movie_name << " was released in " << release_date << " and generated a worldwide gross of around $" << mon_gross << " million." << endl;
+    cout << "Some quotes from this movie include;" << endl;
+    cout << "Miles Morales: I love chai tea." << endl;
+    cout << "Spider-Man India: What did you just say? Chai tea? Chai means \"tea\", bro! You are saying \"tea tea\"! Would I ask you for a \"coffee coffee\" with room for \"cream cream\"?" << endl;
+
+    return 0;
+}
