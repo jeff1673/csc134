dfacc3e42fcc13fdcce7e34a59fa32ec34ba6394
jeff1673 <157391412+jeff1673@users.noreply.github.com>
2024-01-31 16:16:31 +0000

    M2T2 complete

--- M2/M2T2_Fowe.cpp
+++ M2/M2T2_Fowe.cpp
@@ -12,17 +12,23 @@ int main()
 {
     string name;
     int num_apples;
-    double price_per_app = 0.25;
+    double price_per_app;
     double total_cost;
-    cout <<"Hi, what is your name? " "\t\t\t";
+    cout << fixed << setprecision(2);
+    cout <<"Hi, what is your name? ";
     cin >> name;
-    cout << "Welcome to our orchard, " << "\t\t" << name << "." << endl;
-    cout << "Each apple costs " << "\t\t\t$" << price_per_app << "." << endl;
-    cout << "How many apples would you like to get?" "\t";
+    cout << "Welcome to our orchard, " << name << "." << endl;
+    cout << "How much does an apple costs ?" << endl;
+    cin >> price_per_app;
+    cout << "Each apple costs $" << price_per_app << ". How many apples would you like to get?"<< endl;
     cin >> num_apples; 
     total_cost = num_apples * price_per_app;
-    cout << "It will cost " << "\t\t\t\t$" << total_cost << endl;
-    cout << fixed << setprecision(2);
-
+    cout << "It will cost $"  << total_cost << endl;
+    cout << "Here is your receipt" << endl;
+      cout << "----------------------" << endl;
+    cout << "Number of apples:"  "\t" << num_apples << endl;
+    cout << "Price per apple:" "\t$" << price_per_app << endl;
+    cout << "Total:" "\t\t\t$" << total_cost << endl;
+      cout << "----------------------" << endl;
     return 0;
 }
